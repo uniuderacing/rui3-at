679227b81d7ac8a028f04b5fa899d996892bc50d
Francesco Marcato <francescomarcato1@gmail.com>
2023-05-04 17:47:24 +0200

    Tx and rx splt.

--- examples/radio_receive.rs
+++ examples/radio_receive.rs
@@ -1,3 +1,4 @@
+use std::io::BufRead;
 use atat::atat_derive::{AtatCmd, AtatResp};
 
 // static mut INGRESS: Option<atat::IngressManager> = None;
@@ -12,12 +13,19 @@ pub struct NoResponse;
 pub struct AT;
 
 
+
+
 fn main() {
     let mut serial_ports = serialport::available_ports().unwrap();
     let serial_port = serialport::new(serial_ports.pop().unwrap().port_name, 115_200)
         .timeout(std::time::Duration::from_millis(10))
         .open_native()
         .unwrap();
+
+    let (mut tx, mut rx) = serial_port.split();
+    let timer =
+
+    let mut at_client = atat::ClientBuilder::new(tx,  );
 
 }
 
